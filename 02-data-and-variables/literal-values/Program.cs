fc539565e895ec8f2b4cfc4797dceb0d055fc0a5
Ivan A. W. <ivan.aw.1984@icloud.com>
2026-02-07 22:40:12 +0800

    chore(folder): Rename folders with prefix number feat(literal-values): Add literal value project

---
+++ 02-data-and-variables/literal-values/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine('b');
+// Console.WriteLine('Hello World');
+Console.WriteLine(123);
+Console.WriteLine(0.25f);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
