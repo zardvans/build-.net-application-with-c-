a51690722663a00b1f580dc8b2bf58f8e3983799
Ivan A. W. <ivan.aw.1984@icloud.com>
2026-02-11 16:30:34 +0800

    feat(combine-strings): New Project combine string feat(combine-strings): String interpolation

---
+++ 02-data-and-variables/combine-strings/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Concatenate a literal string and a variable
+string firstName = "Bob";
+// string message = "Hello " + firstName;
+// Console.WriteLine(message);
+
+// Concatenate multiple variables and literal strings
+// string greeting = "Hello";
+// string message = greeting + ", " + firstName + "!";
+// Console.WriteLine(message);
+
+// Avoiding intermediate variables
+// Console.WriteLine(greeting + " " + firstName + "!");
+
+// Use string interpolation to combine a literal string and a variable value
+string message = $"Hello {firstName}!";
+Console.WriteLine(message);
+
+// Use string interpolation with multiple variables and literal strings
+int version = 11;
+string updateText = "Update to Windows";
+// string fullMessage = $"{updateText} {version}!";
+// Console.WriteLine(fullMessage);
+
+// Avoid intermediate variables
+Console.WriteLine($"{updateText} {version}!");
+
+// Combine verbatim literals and string interpolation
+string projectName = "First-Project";
+// Console.WriteLine($@"C:\Output\{projectName}\Data");
+Console.WriteLine(@$"C:\Output\{projectName}\Data");
